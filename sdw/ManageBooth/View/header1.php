620357e267a56119620b02a580f0f2cbc4925e55
jianhui <jianhui6019@gmail.com>
2020-07-25 21:11:00 +0800

    add whole project

---
+++ sdw/ManageBooth/View/header1.php
@@ -0,0 +1,62 @@
+<body class="page1" id="top">
+  <div class="main">
+<!--==============================
+              header
+=================================-->
+<header>
+<!--==============================
+            Stuck menu
+=================================-->
+  <div class="container">
+    
+      <div class="grid_12">
+        <div class="clear"></div>
+        <h1>
+          <a href="../View/BoothView.php">
+            <img src="images/logoims.png" alt="Logo alt">
+          </a>
+        </h1>      
+      <div class="clear"></div>  
+    </div>
+
+  </div>
+  <div id="stuck_container">
+    <div class="container">
+      <div class="row">
+        <div class="grid_12">
+        <div class="navigation ">
+          <nav>
+            <ul class="sf-menu">
+            <li class="current"><a href="http://localhost/sdw/ManageLogin/View/CommitteeProfile.php">Home</a></li>
+			<li><a href="/sdw/ManageEvaluation/view/InsertJuryForm.php">Jury</a></li>
+             <li><a href="/sdw/ManageEquipment/manage_equipment/viewequip.php">Equipment</a></li>
+             <li><a href="/sdw/ManageBooth/View/BoothView.php">Booth</a></li>
+			 <li><a href="http://localhost/sdw/ManageLogin/View/">Logout</a></li>
+          </nav>
+      
+          <div class="clear"></div>
+      </div> </div>
+      </div>
+    </div>
+  </div> 
+</header>        
+<div class="slider_wrapper ">
+  <div id="camera_wrap" class="">
+    <div data-src="images/ims1.jpg">
+      <div class="caption fadeIn">
+        Passion in Creating new Spaces
+      </div>
+    </div>
+    <div data-src="images/ims2.jpg">
+      <div class="caption fadeIn">
+        We Build the new World
+      </div>
+    </div>
+    <div data-src="images/ims3.jpg">
+      <div class="caption fadeIn">
+        Great Architecture Solutions
+      </div>
+    </div>
+  </div>
+</div>  
+</body>
